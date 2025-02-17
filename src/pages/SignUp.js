0ffd2c04735c1f4cebb5ef7d5b8007ab90d2b03f
sonukumarbksce1204 <sonukumarbksce1204@gmail.com>
2025-02-17 12:20:42 +0530

    Version 1

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -14,7 +14,9 @@ function SignUp() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/register", {
+      const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+      
+      const response = await fetch(`${API_URL}/api/users/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(user),
